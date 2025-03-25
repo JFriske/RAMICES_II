4650b8050dc73a0a37f35704378a64af3a6c1bfe
JFriske <jennifer.friske.20@ucl.ac.uk>
2025-03-25 15:37:22 +0100

    changed isochrone grid to also us TP-AGB entries

--- src/Stars/IsochroneTracker.cpp
+++ src/Stars/IsochroneTracker.cpp
@@ -43,7 +43,7 @@ void IsochroneTracker::Construct()
 		}
 	}
 	IsoLog("\tSorted Isochrones by metallicity");
-	
+
 	//check temporal grid for uniformity
 	isTimeLogUniform = true;
 	DeltaLogT = CapturedTs[1] - CapturedTs[0];
@@ -233,7 +233,7 @@ void IsochroneTracker::ParseFile(std::string file)
 			
 			
 			int flag = std::stoi(FILE_LINE_VECTOR[9]);
-			if (flag < 8)
+			if (flag < 9) //flag 8 is TPRGB, previously was at < 8 
 			{
 				for (int k = 0; k < PropertyCount; ++k)
 				{
@@ -253,7 +253,7 @@ void IsochroneTracker::ParseFile(std::string file)
 					
 					NewIso = PrevIso;
 					
-					
+
 					
 					for (int k = 0; k < PropertyCount; ++k)
 					{
